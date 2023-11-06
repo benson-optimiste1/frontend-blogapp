cfea2f0e003d29447ff09dfd5d81acb7dc1b5d51
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-06 13:50:08 -0500

    Login add

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer (){
+    return(
+        <footer>
+          <p>
+               <small>
+                Copyright @{new Date().getFullYear()} | All rights reserved
+                </small> 
+            </p>
+        </footer>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+export default function Nav (){
+    return(
+        <header>
+            <ul>
+                <li><a href="/" target="_blanc" rel="noopener noreferrer">Home</a></li>
+                <li><a href="/" target="_blanc" rel="noopener noreferrer">Add Post</a></li>
+                <li><a href="/" target="_blanc" rel="noopener noreferrer">Login</a></li>
+               
+            </ul>
+        
+        </header>
+    )
+}
